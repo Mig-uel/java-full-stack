09486d6ae79f5ee52eb8449ec4920260c0ab77d4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-08 19:22:49 -0500

    Add configuration for 5-example project and update encodings
    - Added pom.xml reference for the 5-example project in misc.xml
    - Updated encodings.xml to include source and resource directories for 5-example
    - Refactored person bean method to use method parameter injection

--- 6-section/5-example/src/main/java/com/example/config/ProjectConfig.java
+++ 6-section/5-example/src/main/java/com/example/config/ProjectConfig.java
@@ -14,13 +14,24 @@ public class ProjectConfig {
         return vehicle;
     }
 
-    @Bean
+ /* @Bean
     Person person() {
         Person person = new Person();
         person.setName("Omar");
 
         // Manually wire the Vehicle bean into the Person bean by calling the vehicle() method
         person.setVehicle(vehicle());
+
+        return person;
+    } */
+
+    @Bean
+    Person person(Vehicle vehicle) { // Method parameter injection
+        Person person = new Person();
+        person.setName("Omar");
+
+        // Manually wire the Vehicle bean into the Person bean by using method parameter
+        person.setVehicle(vehicle);
 
         return person;
     }
