9b8215f2f87ee1998d91f634db7dad95294ee886
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-12 17:52:50 -0500

    Update Vehicle class to comment out Field Injection example
    - Comment out the @Autowired(required = false) annotation
    - Clarify the Setter Injection approach in the Vehicle class

--- 6-section/6-example/src/main/java/com/example/beans/Vehicle.java
+++ 6-section/6-example/src/main/java/com/example/beans/Vehicle.java
@@ -14,8 +14,8 @@ public class Vehicle {
         @Autowired also has a 'required' attribute which is true by default
         If we set required = false, then if there is no bean of type Engine,
         Spring will not throw an exception and will inject null instead
-     */
-    @Autowired(required = false)
+    */
+    // @Autowired(required = false)
     private Engine engine; // Dependency on Engine bean
 
     public Vehicle() {
@@ -38,6 +38,11 @@ public class Vehicle {
         return engine;
     }
 
+    /*
+        This approach is called Setter Injection
+        This approach is also not recommended for production code
+    */
+    @Autowired
     public void setEngine(Engine engine) {
         this.engine = engine;
     }
