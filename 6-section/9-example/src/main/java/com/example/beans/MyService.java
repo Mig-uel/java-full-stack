458d5dd5b21a7b7afdd143b21ece87177508b542
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-12 20:52:43 -0500

    Add lazy initialization to MyService bean definition
    - Updated MyService to use @Lazy annotation for lazy initialization
    - Ensures the bean is created only when requested, optimizing resource usage

--- 6-section/9-example/src/main/java/com/example/beans/MyService.java
+++ 6-section/9-example/src/main/java/com/example/beans/MyService.java
@@ -1,12 +1,14 @@
 package com.example.beans;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Scope("singleton") // This is the default scope, can be omitted
 @Scope(BeanDefinition.SCOPE_SINGLETON) // Explicitly defining singleton scope
+@Lazy // Bean will be lazily initialized // Created only when requested
 public class MyService {
     public MyService() {
         System.out.println("MyService instance created");
