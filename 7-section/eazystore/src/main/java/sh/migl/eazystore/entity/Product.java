e0d422c011223915470c71b19a1ba76f5e5f783e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-13 17:20:20 -0500

    Add Product entity class for JPA mapping
    - Introduces the Product class as a JPA entity
    - Maps to the "products" table with relevant fields
    - Supports persistence for product details in the application

---
+++ 7-section/eazystore/src/main/java/sh/migl/eazystore/entity/Product.java
@@ -0,0 +1,45 @@
+package sh.migl.eazystore.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Entity // Marks this class as a JPA entity
+// An entity is a lightweight, persistent domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
+@Table(name = "products") // Maps this entity to the "products" table in the database
+public class Product {
+    @Id // Specifies the primary key of the entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "popularity", nullable = false)
+    private Integer popularity;
+
+    @Column(name = "image_url", length = 512)
+    private String imageUrl;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    
+}
