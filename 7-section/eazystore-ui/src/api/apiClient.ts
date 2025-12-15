bf7570807939fb82290207f371340e971c1d7023
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 11:51:55 -0500

    Add headers configuration to axios instance
    - Set default Content-Type to application/json
    - Ensures proper handling of JSON requests

--- 7-section/eazystore-ui/src/api/apiClient.ts
+++ 7-section/eazystore-ui/src/api/apiClient.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // Use environment variable for base URL
   timeout: 10000, // 10 seconds timeout
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
