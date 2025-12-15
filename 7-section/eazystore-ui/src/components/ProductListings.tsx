720756006dea7276d46f7f877a0e50dd69f7fb34
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 10:30:28 -0500

    Refactor ProductListings component for improved structure
    - Update product prop type to use Product interface
    - Enhance no products message with styling
    - Replace inline product display with ProductCard component

--- 7-section/eazystore-ui/src/components/ProductListings.tsx
+++ 7-section/eazystore-ui/src/components/ProductListings.tsx
@@ -1,21 +1,22 @@
-export default function ProductListings({
-  products,
-}: {
-  products: Record<string, any>[];
-}) {
+import type { Product } from "../types/product";
+import ProductCard from "./ProductCard";
+
+export default function ProductListings({ products }: { products: Product[] }) {
   if (!products.length) {
-    return <p>No products found</p>;
+    return (
+      <p className="text-center font-primary font-bold text-lg text-primary">
+        No products found
+      </p>
+    );
   }
 
   return (
-    <div>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>Price: ${product.price}</p>
-        </div>
-      ))}
+    <div className="max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
+        {products.map((product) => (
+          <ProductCard key={product.productId} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
