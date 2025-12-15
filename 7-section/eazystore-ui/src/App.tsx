67a0cc9667cc8eadd204e3dec9845ffe7c5d0f8d
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 10:30:45 -0500

    Add Header and Footer components to App layout
    - Enhance the App component structure by including Header and Footer
    - Improve overall layout and user experience

--- 7-section/eazystore-ui/src/App.tsx
+++ 7-section/eazystore-ui/src/App.tsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Home from "./components/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
