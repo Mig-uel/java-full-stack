376a3963de799e7a16fdac286cdab8624e2bdd1b
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 09:00:00 -0500

    Refactor getProducts method to return ProductDto list
    - Updated the getProducts method to return a list of ProductDto
    - Added transformation logic to convert Product entities to ProductDto

--- 7-section/eazystore/src/main/java/sh/migl/eazystore/service/impl/ProductServiceImpl.java
+++ 7-section/eazystore/src/main/java/sh/migl/eazystore/service/impl/ProductServiceImpl.java
@@ -1,7 +1,9 @@
 package sh.migl.eazystore.service.impl;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sh.migl.eazystore.dto.ProductDto;
 import sh.migl.eazystore.entity.Product;
 import sh.migl.eazystore.repository.ProductRepository;
 import sh.migl.eazystore.service.IProductService;
@@ -18,7 +20,17 @@ public class ProductServiceImpl implements IProductService {
     }
 
     @Override
-    public List<Product> getProducts() {
-        return productRepository.findAll();
+    public List<ProductDto> getProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(this::transformToDto)
+                .toList();
+    }
+
+    // Transforms a Product entity to a ProductDto
+    private ProductDto transformToDto(Product product) {
+        ProductDto dto = new ProductDto();
+        BeanUtils.copyProperties(product, dto);
+        return  dto;
     }
 }
