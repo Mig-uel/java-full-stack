53ebef0a6668b2df2784df5da3b985733a6f7b0a
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 18:51:34 -0500

    Add JPA repository and entity scanning annotations
    - Enable JPA repositories and entity scanning in the main application
      class to facilitate proper configuration of data access and
      entity management.

--- 7-section/eazystore/src/main/java/sh/migl/eazystore/EazystoreApplication.java
+++ 7-section/eazystore/src/main/java/sh/migl/eazystore/EazystoreApplication.java
@@ -2,8 +2,12 @@ package sh.migl.eazystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories // Enable JPA Repositories (optional if repositories are in the same package or sub-packages)
+@EntityScan // Enable JPA Entity Scanning (optional if entities are in the same package or sub-packages)
 public class EazystoreApplication {
 
 	public static void main(String[] args) {
