82dd301e87e828f1d0527120428308779f53b5f9
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 09:13:39 -0500

    Remove unnecessary @Repository annotation from ProductRepository
    - The @Repository annotation is not required for Spring Data
      JPA repositories as they are automatically detected.

--- 7-section/eazystore/src/main/java/sh/migl/eazystore/repository/ProductRepository.java
+++ 7-section/eazystore/src/main/java/sh/migl/eazystore/repository/ProductRepository.java
@@ -1,10 +1,7 @@
 package sh.migl.eazystore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import sh.migl.eazystore.entity.Product;
 
-@Repository // Indicates that this interface is a Spring Data repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
