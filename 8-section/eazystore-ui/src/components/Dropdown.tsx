cc4917f0db5ef79870dbaba9ed421fda5b27dec6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 21:19:52 -0500

    Update Dropdown component to include setSelectedOption prop
    - Added setSelectedOption to props for better state management
    - Ensured onChange updates the selected option correctly

--- 8-section/eazystore-ui/src/components/Dropdown.tsx
+++ 8-section/eazystore-ui/src/components/Dropdown.tsx
@@ -2,18 +2,21 @@ export default function Dropdown({
   label,
   options,
   selectedOption,
+  setSelectedOption,
 }: {
   label: string;
   options: string[];
   selectedOption?: string;
+  setSelectedOption: (option: string) => void;
 }) {
   return (
     <div className="flex items-center gap-2 justify-end pr-12 flex-1 font-primary">
       <label className="text-lg font-semibold text-primary">{label}</label>
 
       <select
-        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800"
+        className="px-4 py-2 text-base border rounded-md transition border-primary focus:ring focus:ring-dark focus:outline-none text-gray-800 capitalize"
         value={selectedOption}
+        onChange={(e) => setSelectedOption(e.target.value)}
       >
         {options.map((option) => (
           <option key={option} value={option}>
