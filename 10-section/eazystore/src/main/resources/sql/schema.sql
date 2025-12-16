e3fdd267672a34f949b8561b20aea5d37f1f324f
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-16 16:22:41 -0500

    Add contacts table to schema for user inquiries
    - Introduces a new contacts table to store user messages
    - Enhances the database structure for better user interaction

--- 10-section/eazystore/src/main/resources/sql/schema.sql
+++ 10-section/eazystore/src/main/resources/sql/schema.sql
@@ -10,3 +10,20 @@ CREATE TABLE IF NOT EXISTS products (
     updated_at  TIMESTAMP   DEFAULT NULL,
     updated_by VARCHAR(255) DEFAULT NULL
 );
+
+CREATE TABLE IF NOT EXISTS contacts
+(
+    contact_id bigint auto_increment
+        primary key,
+    name       varchar(100)                          not null,
+    email      varchar(100)                          not null,
+    message    varchar(500)                          not null,
+    phone      varchar(15)                           not null,
+    created_at timestamp default current_timestamp() not null,
+    created_by varchar(50)                           not null,
+    updated_at timestamp default current_timestamp() not null on update current_timestamp(),
+    updated_by varchar(50)                           DEFAULT NULL
+);
+
+
+
