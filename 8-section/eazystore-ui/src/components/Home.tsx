e5ecd3fa02ef9c567b48e2ef12bd9c9350617506
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-15 21:07:53 -0500

    Remove product fetching logic from Home component
    - Simplified Home component by removing the product fetching
      logic and loading/error states.
    - Updated ProductListings to render without Suspense.

--- 8-section/eazystore-ui/src/components/Home.tsx
+++ 8-section/eazystore-ui/src/components/Home.tsx
@@ -1,50 +1,7 @@
-import { Suspense, useEffect, useState } from "react";
-import apiClient from "../api/apiClient";
-import type { Product } from "../types/product";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 
 export default function Home() {
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setIsLoading] = useState<boolean>(true);
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await apiClient.get<Product[]>("/products");
-        setProducts(response.data);
-        setError(null);
-      } catch (error: any) {
-        const err =
-          error.response?.data?.message || "Failed to fetch products.";
-
-        console.error(err);
-        setError(err);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchProducts();
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold text-red-600">{error}</span>
-      </div>
-    );
-  }
-
   return (
     <div className="max-w-6xl mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers">
@@ -52,10 +9,7 @@ export default function Home() {
         unique stickers. Perfect for any occasion!
       </PageHeading>
 
-      {/* TODO: Implement Suspense */}
-      <Suspense fallback={<div>Loading products...</div>}>
-        <ProductListings products={products} />
-      </Suspense>
+      <ProductListings />
     </div>
   );
 }
