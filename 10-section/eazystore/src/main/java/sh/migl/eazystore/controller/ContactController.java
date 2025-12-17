5ba62e8f14f02e0c4fc17f79417002fadd2748f4
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-17 17:57:26 -0500

    Refactor saveContact method to return a success response
    - Changed return type from String to HashMap<String, Boolean>
    - Simplified response handling for contact save operation

--- 10-section/eazystore/src/main/java/sh/migl/eazystore/controller/ContactController.java
+++ 10-section/eazystore/src/main/java/sh/migl/eazystore/controller/ContactController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import sh.migl.eazystore.dto.ContactDto;
 import sh.migl.eazystore.service.IContactService;
 
+import java.util.HashMap;
+
 @RestController // Marks this class as a REST controller
 @RequestMapping("api/v1/contacts") // Base URL for all product-related endpoints
 public class ContactController {
@@ -16,8 +18,10 @@ public class ContactController {
     }
 
     @PostMapping // Handles HTTP POST requests to save a new contact
-    public String saveContact(@RequestBody ContactDto contactDto) {
+    public HashMap<String, Boolean> saveContact(@RequestBody ContactDto contactDto) {
         boolean isSaved = contactService.saveContact(contactDto);
-        return isSaved ? "Contact saved successfully." : "Failed to save contact.";
+        return new HashMap<>() {{
+            put("success", isSaved);
+        }};
     }
 }
