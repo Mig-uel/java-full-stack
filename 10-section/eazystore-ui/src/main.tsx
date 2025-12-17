59390b66ac857acd003c448bc108de46b7aee67e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-12-17 17:47:31 -0500

    Add ToastContainer for notification handling
    - Integrate ToastContainer from react-toastify for user notifications
    - Enhance user experience with customizable toast notifications

--- 10-section/eazystore-ui/src/main.tsx
+++ 10-section/eazystore-ui/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
+import { Bounce, ToastContainer } from "react-toastify";
 import productsLoader from "./loaders/products.loader.ts";
 
+import contactAction from "./actions/contact.action.ts";
 import App from "./App.tsx";
 import About from "./components/About.tsx";
 import Cart from "./components/Cart.tsx";
@@ -11,7 +12,7 @@ import Contact from "./components/Contact.tsx";
 import ErrorPage from "./components/ErrorPage.tsx";
 import Home from "./components/Home.tsx";
 import Login from "./components/Login.tsx";
-import contactAction from "./actions/contact.action.ts";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
@@ -49,5 +50,18 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme={localStorage.getItem("theme") === "dark" ? "dark" : "light"}
+      transition={Bounce}
+    />
   </StrictMode>
 );
